8ad24cf766d01aa821e91ee4edca327e87a9d7df
Benjamin Fogle <benfogle@gmail.com>
2018-04-06 22:19:32 -0400

    Fixes to building libbzip2

--- bzip2.mk
+++ bzip2.mk
@@ -7,23 +7,27 @@ BZIP2 := $(INSTALL)/lib/libbz2.so
 # Bzip2 make file is so simple we'll just do it ourselves, adding the
 # flags that we need. Otherwise we'd need to patch the makefile a bit.
 
-BZIP2_OBJS := \
-	$(BZIP2_EXTRACT)/blocksort.o \
-	$(BZIP2_EXTRACT)/huffman.o    \
-	$(BZIP2_EXTRACT)/crctable.o   \
-	$(BZIP2_EXTRACT)/randtable.o  \
-	$(BZIP2_EXTRACT)/compress.o   \
-	$(BZIP2_EXTRACT)/decompress.o \
-	$(BZIP2_EXTRACT)/bzlib.o
+BZIP2_SRC := \
+	$(BZIP2_EXTRACT)/blocksort.c \
+	$(BZIP2_EXTRACT)/huffman.c    \
+	$(BZIP2_EXTRACT)/crctable.c   \
+	$(BZIP2_EXTRACT)/randtable.c  \
+	$(BZIP2_EXTRACT)/compress.c   \
+	$(BZIP2_EXTRACT)/decompress.c \
+	$(BZIP2_EXTRACT)/bzlib.c
 
-$(BZIP2_OBJS): $(BZIP2_EXTRACT).extracted $(host-toolchain)
+BZIP2_OBJS := $(BZIP2_SRC:.c=.o)
+
+# Have to tell make how to 'build' the source files
+$(BZIP2_SRC): $(BZIP2_EXTRACT).extracted $(host-toolchain)
+
 $(BZIP2_EXTRACT)/%.o: $(BZIP2_EXTRACT)/%.c
 	$(CROSS_CC) $(CROSS_CFLAGS) -O2 -D_FILE_OFFSET_BITS=64 $< -c -o $@
 
 $(BZIP2): $(BZIP2_EXTRACT).extracted $(host-toolchain) $(BZIP2_OBJS)
 	$(CROSS_CC) -shared -Wl,-soname,libbz2.so.1.0 \
 		-o $(dir $@)/libbz2.so.$(BZIP2_VERSION) $(BZIP2_OBJS) $(CROSS_LDFLAGS)
-	ln -s libbz2.so.$(BZIP2_VERSION) $@
+	ln -fs libbz2.so.$(BZIP2_VERSION) $@
 	cp -fd $(BZIP2_EXTRACT)/bzlib.h $(INSTALL)/include
 
 $(compile-host-1): $(BZIP2)
