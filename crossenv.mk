d47dfa104435453bcec06afe400d51c8492e7de8
Benjamin Fogle <benfogle@gmail.com>
2018-11-17 14:26:55 -0500

    Run from master branch of crossenv

--- crossenv.mk
+++ crossenv.mk
@@ -5,9 +5,10 @@ ENV_CROSS_PIP := $(CROSSENV)/cross/bin/pip
 ENV_BUILD_PYTHON := $(CROSSENV)/build/bin/python
 ENV_BUILD_PIP := $(CROSSENV)/build/bin/pip
 ENV_CROSS_EXPOSE := $(CROSSENV)/bin/cross-expose
+CROSSENV_PATH := git+https://github.com/benfogle/crossenv.git
 
 $(CROSSENV_ACTIVATE): $(build-python-interp) $(host-python-interp)
-	$(BUILD_PYTHON) -m pip install crossenv
+	$(BUILD_PYTHON) -m pip install $(CROSSENV_PATH)
 	PATH=$(COMPILE_HOST_PATH) \
 		 $(BUILD_PYTHON) -m crossenv \
 			--sysroot=$(CROSS_SYSROOT) \
